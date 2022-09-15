988cacbc8c6257aa798db8968b7aca9c3261a284
mahajansaurav02 <mahajansaurav02@gmail.com>
2022-09-15 23:31:13 +0530

    model changes

--- src/models/internmodel.js
+++ src/models/internmodel.js
@@ -10,7 +10,8 @@ const internSchema = new mongoose.Schema({
         type: String,
         require: true,
         match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
-        unique: true
+        unique: true,
+        lowercase:true
     },
     mobile: {
         type: String,
